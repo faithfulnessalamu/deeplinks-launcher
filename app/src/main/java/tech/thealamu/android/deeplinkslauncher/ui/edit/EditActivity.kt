7d3b6974b221a43687d93aa87e6962426eb9c0a9
Faithfulness Alamu <vaguemail369@gmail.com>
2021-05-23 14:57:15 +0100

    feat: implement edit activity UI
      - add text inputs for deep link details
      - finish when done is pressed

--- app/src/main/java/tech/thealamu/android/deeplinkslauncher/ui/edit/EditActivity.kt
+++ app/src/main/java/tech/thealamu/android/deeplinkslauncher/ui/edit/EditActivity.kt
@@ -2,11 +2,22 @@ package tech.thealamu.android.deeplinkslauncher.ui.edit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import tech.thealamu.android.deeplinkslauncher.R
+import tech.thealamu.android.deeplinkslauncher.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
+    lateinit var binding: ActivityEditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnDone.setOnClickListener {
+            updateAndFinish()
+        }
+    }
+
+    private fun updateAndFinish() {
+        //TODO: Update repository with data
+        finish()
     }
 }
