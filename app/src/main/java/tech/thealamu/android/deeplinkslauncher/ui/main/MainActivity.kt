432f7c61f087157beb4a7c92d93a5216415aa10f
Faithfulness Alamu <vaguemail369@gmail.com>
2021-05-24 05:52:02 +0100

    feat: init view model in main activity

--- app/src/main/java/tech/thealamu/android/deeplinkslauncher/ui/main/MainActivity.kt
+++ app/src/main/java/tech/thealamu/android/deeplinkslauncher/ui/main/MainActivity.kt
@@ -3,16 +3,26 @@ package tech.thealamu.android.deeplinkslauncher.ui.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import tech.thealamu.android.deeplinkslauncher.data.getAppDatabase
 import tech.thealamu.android.deeplinkslauncher.databinding.ActivityMainBinding
 import tech.thealamu.android.deeplinkslauncher.ui.edit.EditActivity
+import tech.thealamu.android.deeplinkslauncher.ui.edit.EditViewModel
+import tech.thealamu.android.deeplinkslauncher.ui.edit.EditViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Init viewModel
+        val appDao = getAppDatabase(this).deeplinksDao()
+        val viewModelFactory = MainViewModelFactory(appDao)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
 
         binding.content.deeplinksRecyclerview.adapter = DeeplinksListAdapter()
 
---
+++ app/src/main/java/tech/thealamu/android/deeplinkslauncher/ui/main/MainViewModelFactory.kt
@@ -0,0 +1,15 @@
+package tech.thealamu.android.deeplinkslauncher.ui.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import tech.thealamu.android.deeplinkslauncher.data.DeepLinkDao
+
+@Suppress("UNCHECKED_CAST")
+class MainViewModelFactory(val dao: DeepLinkDao): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(dao) as T
+        }
+        throw IllegalArgumentException("viewmodel not found")
+    }
+}
