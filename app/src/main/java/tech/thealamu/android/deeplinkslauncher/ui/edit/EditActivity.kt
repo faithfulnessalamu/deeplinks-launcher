b0503fb95e7313898bcc73e209cbafc80b14e6d0
Faithfulness Alamu <vaguemail369@gmail.com>
2021-05-24 13:52:43 +0100

    feat: implement 'pin to launcher'

--- app/src/main/java/tech/thealamu/android/deeplinkslauncher/ui/edit/EditActivity.kt
+++ app/src/main/java/tech/thealamu/android/deeplinkslauncher/ui/edit/EditActivity.kt
@@ -1,8 +1,13 @@
 package tech.thealamu.android.deeplinkslauncher.ui.edit
 
+import android.content.Intent
+import android.content.pm.ShortcutInfo
+import android.content.pm.ShortcutManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import tech.thealamu.android.deeplinkslauncher.R
@@ -28,8 +33,6 @@ class EditActivity : AppCompatActivity() {
         val viewModelFactory = EditViewModelFactory(appDao)
         viewModel = ViewModelProvider(this, viewModelFactory).get(EditViewModel::class.java)
 
-        registerListeners()
-
         intent?.let { intent ->
             val tmpId = intent.getIntExtra(MainActivity.EXTRA_LINK_ID, -1)
             if (tmpId != -1) {
@@ -44,6 +47,8 @@ class EditActivity : AppCompatActivity() {
             //disable delete feature
             binding.menuItemDelete.visibility = View.GONE
         }
+
+        registerListeners()
     }
 
     private fun registerListeners() {
@@ -62,6 +67,30 @@ class EditActivity : AppCompatActivity() {
             viewModel.deleteDeeplink(DeepLink("", "", "", sessionId))
             finish()
         }
+
+        binding.menuItemAddlauncher.setOnClickListener {
+            sessionId ?: return@setOnClickListener  //no-op
+
+            viewModel.getDeeplink(sessionId!!).observe(this, Observer<DeepLink> {
+                it?.let {
+                    createLauncherIcon(it)
+                }
+            })
+        }
+    }
+
+    private fun createLauncherIcon(d: DeepLink) {
+        val shortcutManager = getSystemService(ShortcutManager::class.java)
+        if (shortcutManager!!.isRequestPinShortcutSupported) {
+            shortcutManager.requestPinShortcut(buildShortcutInfo(d), null)
+        }
+    }
+
+    private fun buildShortcutInfo(d: DeepLink): ShortcutInfo {
+        return ShortcutInfo.Builder(this, d.id.toString())
+            .setShortLabel(d.title)
+            .setIntent(Intent(Intent.ACTION_VIEW, Uri.parse(d.uri)))
+            .build()
     }
 
     private fun populateInputs(sessionId: Int) {
