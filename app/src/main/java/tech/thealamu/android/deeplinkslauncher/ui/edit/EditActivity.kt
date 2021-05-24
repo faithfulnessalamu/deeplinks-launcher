54a001ab4af12fa2e76247da0ba8ef2ce2c7c813
Faithfulness Alamu <vaguemail369@gmail.com>
2021-05-24 05:41:13 +0100

    feat: init edit view model using factory

--- app/src/main/java/tech/thealamu/android/deeplinkslauncher/ui/edit/EditActivity.kt
+++ app/src/main/java/tech/thealamu/android/deeplinkslauncher/ui/edit/EditActivity.kt
@@ -2,14 +2,24 @@ package tech.thealamu.android.deeplinkslauncher.ui.edit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import tech.thealamu.android.deeplinkslauncher.data.AppDatabase
+import tech.thealamu.android.deeplinkslauncher.data.getAppDatabase
 import tech.thealamu.android.deeplinkslauncher.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
     lateinit var binding: ActivityEditBinding
+    lateinit var viewModel: EditViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Init viewModel
+        val appDao = getAppDatabase(this).deeplinksDao()
+        val viewModelFactory = EditViewModelFactory(appDao)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(EditViewModel::class.java)
 
         binding.btnDone.setOnClickListener {
             updateAndFinish()
---
+++ app/src/main/java/tech/thealamu/android/deeplinkslauncher/ui/edit/EditViewModelFactory.kt
@@ -0,0 +1,15 @@
+package tech.thealamu.android.deeplinkslauncher.ui.edit
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import tech.thealamu.android.deeplinkslauncher.data.DeepLinkDao
+
+@Suppress("UNCHECKED_CAST")
+class EditViewModelFactory(val dao: DeepLinkDao): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(EditViewModel::class.java)) {
+            return EditViewModel(dao) as T
+        }
+        throw IllegalArgumentException("viewmodel not found")
+    }
+}
