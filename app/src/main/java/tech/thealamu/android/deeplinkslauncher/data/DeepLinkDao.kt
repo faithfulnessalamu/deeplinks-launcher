8667a55e5b7446e286323fccdc6ed940a7cd9727
Faithfulness Alamu <vaguemail369@gmail.com>
2021-05-24 05:11:04 +0100

    feat: implement database dao

---
+++ app/src/main/java/tech/thealamu/android/deeplinkslauncher/data/DeepLinkDao.kt
@@ -0,0 +1,19 @@
+package tech.thealamu.android.deeplinkslauncher.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface DeepLinkDao {
+    @Query("SELECT * FROM deeplinks")
+    fun getDeeplinks(): LiveData<List<DeepLink>>
+
+    @Insert
+    fun insertDeeplink(d: DeepLink)
+
+    @Update
+    fun updateDeeplink(d: DeepLink)
+
+    @Delete
+    fun deleteDeeplink(d: DeepLink)
+}
